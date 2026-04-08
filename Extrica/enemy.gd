cb57a6c1b0a77ba9bc8281b204c13c60130896ca
Rubix334 <2260334@flourbluffschools.org>
2026-04-08 13:12:16 -0500

    Enemy AI overhaul

--- Extrica/enemy.gd
+++ Extrica/enemy.gd
@@ -1,69 +1,117 @@
-extends PathFollow3D
-
-@onready var navi_agent: NavigationAgent3D = $NavigationAgent3D
-@onready var fov: Area3D = $CharacterBody3D/FOV
-var player_detected = false
-@onready var body: CharacterBody3D = $CharacterBody3D
+extends CharacterBody3D
 
 @export var player:Player
-@export var navimap:NavigationRegion3D
-var speed := 2
+
+@onready var agent: NavigationAgent3D = $NavigationAgent3D
+@onready var vision_ray: RayCast3D = $RayCast3D
+
+@export var patrol_points:Array[Node3D] = []
+@export var speed_walk:float = 1.7
+@export var speed_run:float = 3.0
+@export var attack_range:float = 2.0
+@export var investigate_wait_time:float = 4.0
+@export var patrol_wait_time:float = 3.0
+@export var update_interval:float = 0.2
+
+const UPDATE_TIME = 0.2
+const SPEED = 150
+const SMOOTHING_FACTOR = 0.2
+const VIEW_ANGLE:float = 190.0
+
+enum State {IDLE,PATROL,INVESTIGATE,CHASE,ATTACK,RETURN}
+var state: State = State.IDLE
+
+var target: Node3D
+var patrol_index := 0
+var patrol_timer := 0.0
+var investigate_timer := 0.0
+var update_timer := 0.0
+var investigate_position:Vector3
+var return_position:Vector3
+var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready() -> void:
+	target = player
+	_enter_state(State.IDLE if patrol_points.is_empty() else State.PATROL)
 
 func _physics_process(delta: float) -> void:
-	pass
-
-func _process(delta: float) -> void:
-	patrol(delta)
-
-	body.move_and_slide()
-	pass
-
-func patrol(delta:float):
-	##follow path; check if player is detected
-	##save current position to return to when changing states
-	var last_pos : Vector3
-	if player_detected == false:
-		follow_path(delta)
-	else:
-		last_pos = global_position
-		hunt_player()
-
+	_update_path(delta)
 	
-	
-func hunt_player():
-	##follow player;if player is out of view
-	##start a timer on which timeout will call return_to_patrol()
-	
-	navi_agent.set_target_position(player.global_position)
-	var destination = navi_agent.get_next_path_position()
-	var local_destination = destination - global_position
-	var direction = local_destination.normalized()
-	body.velocity = direction * speed
-	look_at(direction)
-	if player_detected == false:
-		pass
-	
-
-func return_to_patrol():
-	##player is not detected; return to last position of patrolling
-	##change state to patrol
-	pass
-
-
-
-func follow_path(delta):
-	var changeDir = false
-	progress += delta * speed
-	if progress_ratio > 0.99:
-		speed = speed * -1
-	elif progress_ratio < 0.01:
-		speed = speed * -1
+	match state:
+		State.PATROL: _state_patrol(delta)
 		
+	_apply_gravity(delta)
+	move_and_slide()
 
-func detect_player(body: Node3D) -> void:
-	if body is Player:
-		print("Player Detected")
-		player_detected = true
+func _go_to_next_patrol_point() -> void:
+	patrol_index = (patrol_index + 1) % patrol_points.size()
+	agent.set_target_position(patrol_points[patrol_index].global_transform.origin)
+
+func _move_towards(next_pos:Vector3,speed:float) -> void:
+	var dir = (next_pos - global_transform.origin)
+	dir.y = 0.0
+	if is_zero_approx(dir.length()):
+		velocity.x = lerp(velocity.x, 0.0, SMOOTHING_FACTOR)
+		velocity.z = lerp(velocity.z, 0.0, SMOOTHING_FACTOR)
+		return
+	
+	dir = dir.normalized()
+	var current_facing = -global_transform.basis.z
+	var new_dir = current_facing.slerp(dir, SMOOTHING_FACTOR).normalized()
+	look_at(global_transform.origin + new_dir, Vector3.UP)
+	
+	velocity.x = dir.x * speed
+	velocity.z = dir.z * speed
+	
+func _stop_and_idle() -> void:
+	velocity = Vector3.ZERO
+	#anim.play("Idle")
+
+func _walk_to(next_pos: Vector3, speed:float) -> void:
+	#anim.play("Walk")
+	_move_towards(next_pos, speed)
+	
+func _update_agent_target() -> void:
+	match state:
+		State.PATROL:
+			if patrol_points.size() > 0:
+				agent.set_target_position(patrol_points[patrol_index].global_transform.origin)
+		State.INVESTIGATE:
+			agent.set_target_position(investigate_position)
+		State.CHASE:
+			if target:
+				agent.set_target_position(target.global_transform.origin)
+		State.RETURN:
+			agent.set_target_position(return_position)
+
+func _update_path(delta):
+	update_timer -= delta
+	if update_timer <= 0.0:
+		_update_agent_target()
+		update_timer = update_interval
+
+func _apply_gravity(delta:float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 	else:
-		player_detected = false
-		print("Player out of view")
+		velocity.y = 0.0
+	
+func _enter_state(new_state: State) -> void:
+	state = new_state
+	match state:
+		State.PATROL:
+			patrol_timer = 0
+			_go_to_next_patrol_point()
+
+func _state_patrol(delta:float) -> void:
+	if agent.is_navigation_finished():
+		if patrol_timer <= 0.0:
+			patrol_timer = patrol_wait_time
+			_stop_and_idle()
+		else:
+			patrol_timer -= delta
+			if patrol_timer <= 0.0:
+				_go_to_next_patrol_point()
+	else:
+		_walk_to(agent.get_next_path_position(),speed_walk)
+		
