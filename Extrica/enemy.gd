dd80e05667c029ba5203be7ffef35523811c6cfb
Rubix334 <2260334@flourbluffschools.org>
2026-04-09 12:36:44 -0500

    Enemy AI ALMOST DONE. Weird bug when player is seen

--- Extrica/enemy.gd
+++ Extrica/enemy.gd
@@ -38,8 +38,13 @@ func _physics_process(delta: float) -> void:
 	_update_path(delta)
 	
 	match state:
+		State.IDLE:   _state_idle()
 		State.PATROL: _state_patrol(delta)
-		
+		State.CHASE:  _state_chase(delta)
+		State.ATTACK: _state_attack()
+		State.RETURN: _state_return(delta)
+	
+	_looking()
 	_apply_gravity(delta)
 	move_and_slide()
 
@@ -95,13 +100,39 @@ func _apply_gravity(delta:float) -> void:
 		velocity.y -= gravity * delta
 	else:
 		velocity.y = 0.0
+
+func _can_see_player() -> bool:
+	return target and vision_ray.is_colliding() and vision_ray.get_collider() == target
+
+func _looking() -> void:
+	if not target:
+		return 
 	
+	var to_player = (target.global_transform.origin - global_transform.origin).normalized()
+	var forward = -global_transform.basis.z
+	var angle_deg = rad_to_deg(acos(clamp(forward.dot(to_player), -1.0, 1.0)))
+	if angle_deg > VIEW_ANGLE * 0.5:
+		return
+	
+	var ray_forward = -vision_ray.global_transform.basis.z
+	var new_dir = ray_forward.slerp(to_player, SMOOTHING_FACTOR).normalized()
+	vision_ray.look_at(vision_ray.global_transform.origin + new_dir, Vector3.UP)
+
 func _enter_state(new_state: State) -> void:
 	state = new_state
 	match state:
 		State.PATROL:
 			patrol_timer = 0
 			_go_to_next_patrol_point()
+		State.INVESTIGATE:
+			investigate_timer = 0.0
+			agent.set_target_position(investigate_position)
+		State.CHASE, State.INVESTIGATE:
+			return_position = global_transform.origin
+
+func _state_idle() -> void:
+	if _can_see_player():
+		_enter_state(State.CHASE)
 
 func _state_patrol(delta:float) -> void:
 	if agent.is_navigation_finished():
@@ -114,4 +145,56 @@ func _state_patrol(delta:float) -> void:
 				_go_to_next_patrol_point()
 	else:
 		_walk_to(agent.get_next_path_position(),speed_walk)
+	
+	if _can_see_player():
+		_enter_state(State.CHASE)
+
+func _state_chase(delta: float) -> void:
+	if not target:
+		_enter_state(State.RETURN)
+		return
+	
+	_walk_to(agent.get_next_path_position(), speed_run)
+	
+	if global_transform.origin.distance_to(target.global_transform.origin) < attack_range:
+		_enter_state(State.ATTACK)
+	elif not _can_see_player():
+		investigate_position = target.global_transform.origin
+		_enter_state(State.INVESTIGATE)
+	
+func _state_attack() -> void:
+	#velocity = Vector3.ZERO
+	#anim.play("attack")
+	#kill player and reset
+	_enter_state(State.CHASE)
+	pass
+	
+
+func _state_investigate(delta:float) -> void:
+	if agent.is_navigation_finished():
+		if investigate_timer <= 0.0:
+			investigate_timer = investigate_wait_time
+			_stop_and_idle()
+		else:
+			investigate_timer -= delta
+			if investigate_timer <= 0.0:
+				_enter_state(State.RETURN)
+	else:
+		_walk_to(agent.get_next_path_position(),speed_walk)
+	
+	if _can_see_player():
+		_enter_state(State.CHASE)
+
+func _state_return(delta:float) -> void:
+	if agent.is_navigation_finished():
+		_enter_state(State.PATROL)
+	else:
+		_walk_to(agent.get_next_path_position(), speed_walk)
 		
+	if _can_see_player():
+		_enter_state(State.CHASE)
+
+func hear_noise(pos:Vector3) -> void:
+	if state not in [State.CHASE, State.ATTACK]:
+		investigate_position = pos
+		_enter_state(State.INVESTIGATE)
